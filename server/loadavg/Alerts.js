5b4ede78dbb67c3d409d903496d3664d14bb2229
Olivier Scherrer <pode.fr@gmail.com>
2016-04-15 00:06:15 -0400

    SERVER-02 Add unit tests for alerts

---
+++ server/loadavg/Alerts.js
@@ -0,0 +1,141 @@
+"use strict";
+
+const bacon = require("baconjs");
+const moment = require("moment");
+
+/**
+ * Computes alerts on a N minutes window of load averages.
+ * When there are enough load averages and the average is greater then A,
+ * then an alert is triggered. When the average goes back below 1, the alert is lifted.
+ *
+ * @param {Stream} stream a bacon stream to get the load averages from
+ * @param {Function} onAlert a callback to be called when an alert triggers
+ * @constructor
+ */
+module.exports = function Alerts(stream, onAlert) {
+    if (!stream) {
+        throw new Error("Alerts requires a Bacon stream when instanciated");
+    }
+
+    if (typeof onAlert !== "function") {
+        throw new Error("Alerts requires an onAlert callback when instanciated");
+    }
+
+    const ALERT_RAISED = "ALERT_RAISED";
+    const ALERT_CLEARED = "ALERT_CLEARED";
+
+    /**
+     * The size of the time window
+     * @type {Array} a tuple with a number and a unit, used by moment.js
+     * @private
+     */
+    let _timeWindow = [1, "minute"];
+
+    /**
+     * The callback when an alert is triggered
+     * @type {Function} callback
+     * @private
+     */
+    let _onAlert = onAlert;
+
+    /**
+     * The unix timestamp when the stream starts so that we know when we can start triggering events
+     * @type {Number}
+     * @private
+     */
+    let _startDate;
+
+    /**
+     *
+     * @type {number}
+     * @private
+     */
+    let _threshold = 1;
+
+    /**
+     * Set a time window to calculate averages on. All events that were triggered outside of the window
+     * will be discarded. Also, the first alert will only be triggered when the oldest event is older than the window,
+     * so that we're sure that we're triggering it based on an average from an relevant time window.
+     *
+     * @example
+     * setAverageTimeWindow(2, "minutes");
+     * setAverageTimeWindow(30, "seconds");
+     * setAverageTimeWindow(1, "hour");
+     *
+     * @param {Number} number the number of units
+     * @param {String} unit the unit (minutes, hours, seconds...)
+     */
+    this.setAverageTimeWindow = function setAverageTimeWindow(number, unit) {
+        if (typeof number !== "number" || typeof unit !== "string") {
+            throw new Error(`setAverageTimeWindow needs to be called with a number and a string that represents \
+                a time period. For instance: setAverageTimeWindow(2, "minutes")`);
+        }
+
+        _timeWindow = [number, unit];
+    };
+
+    /**
+     * Set the threshold above and below which alerts are triggered
+     * @param {Number} threshold the threshold above and below which alerts are triggered
+     */
+    this.setThreshold = function setThreshold(threshold) {
+        if (typeof threshold !== "number") {
+            throw new Error("setThreshold needs to be called with a number representing the load average that triggers alerts");
+        }
+
+        _threshold = threshold;
+    };
+
+    /**
+     * Initializes the alerts computation. And alert is triggered when the threshold is reached, then the onAlert callback is called.
+     */
+    this.init = function init() {
+        _startDate = moment().unix();
+
+        let hasNotEnoughData = getNotEnoughDataProperty(stream);
+
+        stream
+            .scan([], accumulateLoadAverage)
+            .toEventStream()
+            .map(calculateAverage)
+            .skipWhile(hasNotEnoughData)
+            .map(computeAlert)
+            .skipDuplicates()
+            .skipWhile(cantTriggerAlert)
+            .map(_onAlert)
+            .log();
+    };
+
+    function getNotEnoughDataProperty(stream) {
+        return stream.map(data => {
+            return _startDate > moment(data.time).subtract(_timeWindow[0], _timeWindow[1]).unix();
+        }).toProperty();
+    }
+
+    function accumulateLoadAverage(accumulator, data) {
+        accumulator.push(data);
+        return accumulator.filter(item => !isOlderThanLimit(item.time));
+    }
+
+    function calculateAverage(accumulatedLoadAverage) {
+        const sum = accumulatedLoadAverage.reduce((sum, item) => sum + item.loadAverage[0], 0);
+
+        return sum / accumulatedLoadAverage.length;
+    }
+
+    function isOlderThanLimit(time) {
+        return moment(time).unix() < moment().subtract(_timeWindow[0], _timeWindow[1]).unix();
+    }
+
+    function computeAlert(average) {
+        return {
+            load: average,
+            time: moment().format(),
+            type: average >= _threshold ? ALERT_RAISED : ALERT_CLEARED
+        };
+    }
+
+    function cantTriggerAlert(alert) {
+        return alert.type === ALERT_CLEARED;
+    }
+};
--- server/loadavg/tests/alerts.spec.js
+++ server/loadavg/tests/alerts.spec.js
@@ -1,9 +1,146 @@
 "use strict";
 
-describe("Given initialized load average", _ => {
-    describe("When getting load average data", function () {
-       it("Then computes the average load average for the last 2 minutes", function () {
+const bacon = require("baconjs");
+const moment = require("moment");
 
-       });
+const Alerts = require("../Alerts");
+
+describe("Given a load average stream And an onAlert callback", function () {
+    let stream;
+    let sink;
+    let onAlert;
+
+    beforeEach(function () {
+        stream = bacon.fromBinder(_sink => {
+            sink = _sink;
+        });
+
+        onAlert = jasmine.createSpy();
+    });
+
+    describe("And Alerts is initialized with a 30 seconds window and a threshold of 2", function () {
+        let alerts;
+
+        beforeEach(function () {
+            alerts = new Alerts(stream, onAlert);
+
+            alerts.setAverageTimeWindow(30, "seconds");
+            alerts.setThreshold(2);
+
+            alerts.init();
+        });
+
+        describe("When the first load average events come in", function () {
+            beforeEach(function () {
+                [
+                    {
+                        loadAverage: [2.5, 3, 2],
+                        time: moment().format()
+                    },
+                    {
+                        loadAverage: [2.5, 3, 2],
+                        time: moment().add(20, "seconds").format()
+                    }
+                ].map(event => sink(event));
+            });
+
+            it("Then no alert is triggered", function () {
+                expect(onAlert).not.toHaveBeenCalled();
+            });
+        });
+
+        describe("When there are enough events to cover the time window", function () {
+            beforeEach(function () {
+                [
+                    {
+                        loadAverage: [2.2, 3, 2],
+                        time: moment().format()
+                    },
+                    {
+                        loadAverage: [1.9, 3, 2],
+                        time: moment().add(20, "seconds").format()
+                    },
+                    {
+                        loadAverage: [1.7, 3, 2],
+                        time: moment().add(40, "seconds").format()
+                    }
+                ].map(event => sink(event));
+            });
+
+            describe("And the average is below the threshold", function () {
+                it("Then no alert is triggered (there's no alert to clear)", function () {
+                    expect(onAlert).not.toHaveBeenCalled();
+                });
+
+                describe("When the average crosses the threshold", function () {
+                    beforeEach(function () {
+                        [
+                            {
+                                loadAverage: [2.2, 3, 2],
+                                time: moment().format()
+                            },
+                            {
+                                loadAverage: [2.3, 3, 2],
+                                time: moment().add(60, "seconds").format()
+                            }
+                        ].map(event => sink(event));
+                    });
+
+                    it("Then an alert is triggered", function () {
+                        expect(onAlert).toHaveBeenCalledWith({
+                            load: 2,
+                            time: moment().format(),
+                            type: "ALERT_RAISED"
+                        });
+                    });
+                });
+            });
+
+            describe("And the average is above the threshold", function () {
+                beforeEach(function () {
+                    [
+                        {
+                            loadAverage: [2.2, 3, 2],
+                            time: moment().format()
+                        },
+                        {
+                            loadAverage: [2.3, 3, 2],
+                            time: moment().add(60, "seconds").format()
+                        }
+                    ].map(event => sink(event));
+                });
+
+                it("Then an alert is triggered", function () {
+                    expect(onAlert).toHaveBeenCalledWith({
+                        load: 2.06,
+                        time: moment().format(),
+                        type: "ALERT_RAISED"
+                    });
+                });
+
+                describe("When the average goes below the threshold", function () {
+                    beforeEach(function () {
+                        [
+                            {
+                                loadAverage: [1.5, 3, 2],
+                                time: moment().format()
+                            },
+                            {
+                                loadAverage: [1, 3, 2],
+                                time: moment().add(60, "seconds").format()
+                            }
+                        ].map(event => sink(event));
+                    });
+
+                    it("Then the alert is cleared", function () {
+                        expect(onAlert).toHaveBeenCalledWith({
+                            load: 1.8285714285714287,
+                            time: moment().format(),
+                            type: "ALERT_CLEARED"
+                        });
+                    });
+                });
+            });
+        });
     });
 });
