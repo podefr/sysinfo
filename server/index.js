d0544ea2d7afb84e2fa98cce9c55b28c12eaee96
Olivier Scherrer <pode.fr@gmail.com>
2016-04-15 22:42:16 -0400

    SERVER-02 fix indentation

--- server/index.js
+++ server/index.js
@@ -18,52 +18,52 @@ initIO();
 startModules();
 
 function startModules() {
-   Object.keys(configuration.SERVER.MODULES).forEach(moduleName => {
-      const moduleConfiguration = configuration.SERVER.MODULES[moduleName];
-      const module = require(`./${moduleName}/index`);
-      const moduleLevelConfiguration = require(`./${moduleName}/config/${moduleName}.conf.json`);
+    Object.keys(configuration.SERVER.MODULES).forEach(moduleName => {
+        const moduleConfiguration = configuration.SERVER.MODULES[moduleName];
+        const module = require(`./${moduleName}/index`);
+        const moduleLevelConfiguration = require(`./${moduleName}/config/${moduleName}.conf.json`);
 
-      module.init(connectToAgent(moduleName), serverSocket.of(`${moduleName}`), moduleConfiguration);
+        module.init(connectToAgent(moduleName), serverSocket.of(`${moduleName}`), moduleConfiguration);
 
-      if (moduleLevelConfiguration && moduleLevelConfiguration.ROUTES) {
-         let moduleRoutes = moduleLevelConfiguration.ROUTES;
+        if (moduleLevelConfiguration && moduleLevelConfiguration.ROUTES) {
+            let moduleRoutes = moduleLevelConfiguration.ROUTES;
 
-         Object.keys(moduleRoutes).forEach(routeName => {
-            const methodName = moduleRoutes[routeName];
+            Object.keys(moduleRoutes).forEach(routeName => {
+                const methodName = moduleRoutes[routeName];
 
-            if (typeof module[methodName] !== "function") {
-               throw new Error(`No handler ${moduleName}.${methodName} found for route ${moduleName}/${routeName}`);
-            }
-            application.get(`/${moduleName}/${routeName}`, module[methodName].bind(module));
-         });
-      }
+                if (typeof module[methodName] !== "function") {
+                    throw new Error(`No handler ${moduleName}.${methodName} found for route ${moduleName}/${routeName}`);
+                }
+                application.get(`/${moduleName}/${routeName}`, module[methodName].bind(module));
+            });
+        }
 
-      console.log(`${moduleName} started`);
-   });
+        console.log(`${moduleName} started`);
+    });
 }
 
 function connectToAgent(moduleName) {
-   const URL = configuration.AGENT.URL;
-   const PORT = configuration.AGENT.PORT;
-   const socket = ioClient.connect(`${URL}:${PORT}/${moduleName}`);
+    const URL = configuration.AGENT.URL;
+    const PORT = configuration.AGENT.PORT;
+    const socket = ioClient.connect(`${URL}:${PORT}/${moduleName}`);
 
-   socket.on("connect", _ => console.log(`${moduleName} is connected`));
-   socket.on("disconnect", _ => console.log(`${moduleName} is disconnected`));
+    socket.on("connect", _ => console.log(`${moduleName} is connected`));
+    socket.on("disconnect", _ => console.log(`${moduleName} is disconnected`));
 
-   return socket;
+    return socket;
 }
 
 function startWebServer() {
-   const port = configuration.SERVER.PORT;
+    const port = configuration.SERVER.PORT;
 
-   application.get("/", (request, response) => {
-      response.sendFile(path.resolve(`${__dirname}/${configuration.SERVER.INDEX_PATH}`));
-   });
+    application.get("/", (request, response) => {
+        response.sendFile(path.resolve(`${__dirname}/${configuration.SERVER.INDEX_PATH}`));
+    });
 
-   server.listen(port);
-   console.log(`Web server running on http://localhost:${port}`);
+    server.listen(port);
+    console.log(`Web server running on http://localhost:${port}`);
 }
 
 function initIO() {
-   serverSocket.on("connect", _ => console.log("Accepted new connection from web client"));
+    serverSocket.on("connect", _ => console.log("Accepted new connection from web client"));
 }
